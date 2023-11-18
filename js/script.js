3f10441921a4d7d13819a0fdbb155a260bb8a4e2
Kaige_Wei <89125519+Kaige-Wei-425@users.noreply.github.com>
2023-11-19 09:00:32 +1100

    add sidebar to header

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function sidebar_open() {
+    document.getElementById("sidebar").style.display = "block";
+}
+function sidebar_close() {
+    document.getElementById("sidebar").style.display = "none";
+}
